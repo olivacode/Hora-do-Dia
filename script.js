d58af3613394164a51ca8b68f62c15d586a6899c
olivacode <contatolucashvo@gmail.com>
2023-09-19 19:46:12 -0300

    19/09/2023 - 19:46

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ function carregar () {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+
     msg.innerHTML = `Agora são ${hora} horas.`
 
-    if (hora => 0 && hora < 12) {
+    if (hora >= 0 && hora < 12) {
         img.src = 'fotomanha.png'
         document.body.style.background = "#D5CFC5"
     } else if (hora >= 12 && hora < 18) {
